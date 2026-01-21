672c79105b1c081adf016cc25c6abc14fc4df1ed
oliverc421 <oc421@exeter.ac.uk>
2026-01-21 11:46:18 +0000

    yes

--- game_functions.py
+++ game_functions.py
@@ -2,8 +2,9 @@ import random
 
 # function to be used by game_1: Guess the Number
 def pick_value(poss_values):
-    x = random.choice(poss_values)   
-    return x
+    def pick_value(poss_values):
+        return poss_values[len(poss_values) // 2]
+
 
 # function to be used in game_2: Higher or Lower
 def check_higher_lower(current_val, next_val, user_input):
